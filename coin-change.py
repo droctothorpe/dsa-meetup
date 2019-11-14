bcca43530d60a2d144c64cc7956313608685c9ec
Alexander Perlman <alex@Alexanders-MacBook-Pro.local>
2019-11-14 07:05:56 -0500

    WIP

--- coin-change.py
+++ coin-change.py
@@ -1,4 +1,4 @@
-def greedy(coins, amount):
+def greedy_subtract(coins, amount):
     # Guardians
     if amount == 0:
         return 0
@@ -8,8 +8,23 @@ def greedy(coins, amount):
     count = 0
     for coin in coins:
         while coin <= amount:
-            amount -= coin # Refactor to divide?
+            amount -= coin
             count += 1
+    return count if amount == 0 else -1
+
+
+def greedy_divide(coins, amount):
+    # Guardians
+    if amount == 0:
+        return 0
+
+    coins = sorted(coins)
+    coins.reverse()
+    count = 0
+    for coin in coins:
+        quotient = int(amount / coin)
+        count += quotient
+        amount -= quotient * coin
     return count if amount == 0 else -1
 
 
@@ -19,15 +34,43 @@ def bottom_up(coins, amount):
         return 0
 
     coins = sorted(coins)
-    dp = [amount + 1] * (amount + 1) 
+    dp = [amount + 1] * (amount + 1)
     dp[0] = 0
-    for i in range(len(dp)): 
-        for coin in coins: 
+    for i in range(len(dp)):
+        for coin in coins:
             if coin <= i:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[amount] if dp[amount] <= amount else -1
 
 
+def top_down(coins, amount):
+    min_coins = amount
+    if amount == 0:
+        return 0 
+    if amount in coins:
+        return 1
+    for coin in coins:
+        if coin <= amount:
+            num_coins = 1 + top_down(coins, amount - coin)
+            if num_coins < min_coins:
+                min_coins = num_coins
+    return min_coins
+
+
+def recursive(coins, amount):
+    min_coins = amount
+    if amount in coins:
+        return 1  # base case
+    else:
+        for coin in [coin for coin in coins if coin <= amount]:
+            num_coins = 1 + recursive(coins, amount - coin)
+            if num_coins < min_coins:
+                min_coins = num_coins
+    return min_coins
+
+
 c, a = [1, 2, 5], 11
-# c, a = [4, 5], 8 
-print(bottom_up(c, a))
+# c, a = [1, 2, 3], 5
+c, a = [6, 5], 8
+c, a = [100, 1], 107
+print(recursive(c, a))
